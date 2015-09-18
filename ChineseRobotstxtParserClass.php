0752977edb86efb4df7dbc47f1d6d2bd0fbbe81c
seodug <frkmain@gmail.com>
2015-09-18 10:18:26 +0800

    Minimal format change

--- ChineseRobotstxtParserClass.php
+++ ChineseRobotstxtParserClass.php
@@ -51,14 +51,10 @@ class ChineseRobotsTxtParser {
 
 			if(preg_match('/user\-agent\s*\:\s*(.+)/i', $row, $matches)){
 				$curUA=$matches[1];
-				if (!isset($this->parsedRule[$curUA])) {
-					$this->parsedRule[$curUA]=array('allow'=>array(),'disallow'=>array());
-				}
+				if (!isset($this->parsedRule[$curUA])) $this->parsedRule[$curUA]=array('allow'=>array(),'disallow'=>array());
 			} elseif(preg_match('/(allow|disallow)\s*\:\s*(.+)/i', $row, $matches)){
 				// 非法规则需要直接过滤。非法规则：指令中包含空格||指令不是以/开头||指令内容为空
-				if (preg_match('/\s/', $matches[2]) || !preg_match('/^\//', $matches[2]) || empty($matches[2])) {
-					continue;
-				}
+				if (preg_match('/\s/', $matches[2]) || !preg_match('/^\//', $matches[2]) || empty($matches[2])) continue;
 				$curRule=$matches[2];
 				$curRule=str_replace('/', '\/', $curRule);
 				$curRule=str_replace('?', '\?', $curRule);
@@ -94,9 +90,7 @@ class ChineseRobotsTxtParser {
 	//对给定的URL或URI判断是否允许抓取
 	public function isAllowed($value='/',$userAgent='*')
 	{
-		if ($this->invalidRobotstxt) {
-			return true;
-		}
+		if ($this->invalidRobotstxt) return true;
 		//如果没有设置UA且不是*，则直接调用*的默认规则用于判断
 		if (!isset($this->parsedRule[$userAgent])){
 			if ($userAgent!='*') {
@@ -130,9 +124,7 @@ class ChineseRobotsTxtParser {
 							$ruleA_length=strlen($ruleRegex)>$ruleA_length?strlen($ruleRegex):$ruleA_length;
 						}
 					}
-					if ($isA) {
-						return $ruleA_length>=$ruleD_length;
-					} 
+					if ($isA) return $ruleA_length>=$ruleD_length; 
 				}
 			} else {
 				return true;
